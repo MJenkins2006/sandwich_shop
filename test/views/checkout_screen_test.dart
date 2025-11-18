3c13afcda9e9e1d07c363d7b8894702dc49a9d83
Matthew Jenkins <matthew.t.jenkins9@gmail.com>
2025-11-18 12:34:50 +0000

    Update checkout_screen_test.dart

--- test/views/checkout_screen_test.dart
+++ test/views/checkout_screen_test.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:sandwich_shop/views/checkout_screen.dart';
 import 'package:sandwich_shop/models/cart.dart';
-import 'package:sandwich_shop/models/sandwich.dart';
 
 void main() {
   TestWidgetsFlutterBinding.ensureInitialized();
