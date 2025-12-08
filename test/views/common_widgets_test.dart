bc9b20abd800bcf21ee2dde9cfd969980d88c2c0
Matthew Jenkins <matthew.t.jenkins9@gmail.com>
2025-12-08 15:56:20 +0000

    Create common_widgets appbar tests

--- test/views/common_widgets_test.dart
+++ test/views/common_widgets_test.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:sandwich_shop/main.dart';
+import 'package:sandwich_shop/views/order_screen.dart';
 
 void main() {
   group('Drawer tests', () {
@@ -41,6 +42,49 @@ void main() {
       expect(find.text('Cart'), findsWidgets);
       expect(find.text('Checkout'), findsWidgets);
 
+    });
+  });
+  group('AppBar tests', () {
+    testWidgets('AppBar shows all items', (WidgetTester tester) async {
+      await tester.pumpWidget(const App());
+
+      // Check for AppBar title on the home screen
+      expect(find.text('Sandwich Counter'), findsWidgets);
+
+      // Check for shopping cart icon
+      expect(find.byIcon(Icons.shopping_cart), findsWidgets);
+    });
+  
+    testWidgets('Cart amount changes', (WidgetTester tester) async {
+      await tester.pumpWidget(const App());
+
+      // Shopping cart icon should display 0 items initially then 4 after adding items
+      expect(find.text('0'), findsWidgets);
+      final Finder adder = find.widgetWithText(StyledButton, 'Add to Cart');
+      await tester.ensureVisible(adder);
+      await tester.tap(adder);
+      await tester.pumpAndSettle();
+      await tester.tap(adder);
+      await tester.pumpAndSettle();
+      await tester.tap(adder);
+      await tester.pumpAndSettle();
+      await tester.tap(adder);
+      await tester.pumpAndSettle();
+
+      // open the drawer via the menu icon
+      final Finder menuButton = find.byIcon(Icons.menu);
+      expect(menuButton, findsWidgets);
+      await tester.tap(menuButton.first);
+      await tester.pumpAndSettle();
+
+      // Tap the Sign In item
+      final Finder signInTile = find.text('Sign In');
+      expect(signInTile, findsWidgets);
+      // choose the drawer tile version (ListTile inside Drawer)
+      await tester.tap(signInTile.first);
+      await tester.pumpAndSettle();
+
+      expect(find.text('4'), findsWidgets);
     });
   });
 }
