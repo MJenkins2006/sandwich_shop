da262c9b554d4d89628fae1ea13b67074629c5bf
Matthew Jenkins <matthew.t.jenkins9@gmail.com>
2025-12-08 17:44:38 +0000

    Add settings_screen slider test

--- test/views/settings_screen_test.dart
+++ test/views/settings_screen_test.dart
@@ -24,5 +24,40 @@ void main() {
       expect(AppStyles.baseFontSize, 20.0);
     });
 
+    testWidgets('updates font size when slider moved and saves to prefs',
+        (WidgetTester tester) async {
+      SharedPreferences.setMockInitialValues({'fontSize': 16.0});
+
+      await tester.pumpWidget(
+        ChangeNotifierProvider<Cart>(
+          create: (_) => Cart(),
+          child: const MaterialApp(home: SettingsScreen()),
+        ),
+      );
+      await tester.pumpAndSettle();
+
+      final sliderFinder = find.byType(Slider);
+      expect(sliderFinder, findsOneWidget);
+
+      final RenderBox sliderBox = tester.firstRenderObject(sliderFinder);
+      final Offset sliderTopLeft = sliderBox.localToGlobal(Offset.zero);
+      final double sliderWidth = sliderBox.size.width;
+
+      const double targetValue = 22.0;
+      const double min = 12.0;
+      const double max = 24.0;
+      const double fraction = (targetValue - min) / (max - min);
+
+      final Offset tapOffset =
+          sliderTopLeft + Offset(sliderWidth * fraction, sliderBox.size.height / 2);
+
+      await tester.tapAt(tapOffset);
+      await tester.pumpAndSettle();
+
+      final SharedPreferences prefs = await SharedPreferences.getInstance();
+      expect(prefs.getDouble('fontSize'), equals(targetValue));
+      expect(AppStyles.baseFontSize, equals(targetValue));
+      expect(find.text('Current size: ${targetValue.toInt()}px'), findsOneWidget);
+    });
   });
 }
