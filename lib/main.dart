f7f3b20a11994c5ac58ad35c51f5f822f4c1f918
Matthew Jenkins <matthew.t.jenkins9@gmail.com>
2025-12-18 12:46:14 +0000

    Configure SQLite for desktop

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'dart:io' show Platform;
+import 'package:flutter/foundation.dart' show kIsWeb;
+import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 import 'package:provider/provider.dart';
 import 'package:sandwich_shop/models/cart.dart';
 import 'package:sandwich_shop/views/order_screen.dart';
@@ -6,6 +9,12 @@ import 'package:sandwich_shop/views/app_styles.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  // Initialize sqflite for desktop platforms. This sets the global
+  // `databaseFactory` used by `sqflite` to the ffi-backed implementation.
+  if (!kIsWeb && (Platform.isWindows || Platform.isLinux || Platform.isMacOS)) {
+    sqfliteFfiInit();
+    databaseFactory = databaseFactoryFfi;
+  }
   await AppStyles.loadFontSize();
   runApp(const App());
 }
